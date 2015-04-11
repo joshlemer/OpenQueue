62456c67bc2e7afd2642766dc1632f3c3ac971b1
Josh Lemer <joshlemer@gmail.com>
2015-04-11 15:25:19 -0500

    Add models and templates

---
+++ models.py
@@ -0,0 +1,26 @@
+import datetime
+from flask import url_for
+from qme_src import db
+
+class Resource(db.EmbeddedDocument):
+	created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+	name = db.StringField(max_length=255, required=True)
+
+class Room(db.Document):
+	created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+	name = db.StringField(max_length=255, required=True)
+	slug = db.StringField(max_length=255, required=True)
+	resources = db.ListField(db.EmbeddedDocumentField('Resource'))
+
+	def get_absolute_url(self):
+		return url_for('room', kwargs={"slug": self.slug})
+
+	def __unicode__(self):
+		return self.name
+
+	meta = {
+		'allow_inheritance': True,
+		'indexes': ['slug'],
+		'ordering': ['slug']
+	}
+
