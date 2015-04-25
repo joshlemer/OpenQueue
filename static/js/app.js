dcfbb6a1275928d444f1a63757ed618da1b203b6
shivani <tashivani@gmail.com>
2015-04-25 17:57:36 -0500

    Got room names from api

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,16 @@
-angular.module('app', []);
+var app = angular.module('app', [])
+	.config(function($interpolateProvider){
+	    $interpolateProvider.startSymbol('[[').endSymbol(']]');
+	});
+
+app.controller("RoomController", function(){
+    this.title = 'AngularJS Works!';
+});
+
+app.controller('RoomAPIController', ['$http',function($http){
+	var value = this;
+	value.title = [];
+	$http.get('/api/rooms/room_1/').success(function(data){
+		value.data = data;
+	});
+}]);
--- static/js/main.ctrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('app').controller("RoomController", function(){
-    this.title = 'AngularJS Works!';
-});
