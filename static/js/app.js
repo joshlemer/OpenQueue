08ea6fb9c7d9dc3f8678aab2ef91737b1f1ef4ce
Josh Lemer <joshlemer@gmail.com>
2015-04-27 22:15:45 -0500

    Add angular routes

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,25 @@
-var app = angular.module('app', [])
+var app = angular.module('app', ['ngRoute'])
 	.config(function($interpolateProvider){
 	    $interpolateProvider.startSymbol('[[').endSymbol(']]');
-	});
 
-app.controller("RoomController", function(){
-    this.title = 'AngularJS Works!';
-});
-
-app.controller('RoomAPIController', ['$http',function($http){
-	var value = this;
-	value.title = [];
-	$http.get('/api/rooms/room_1/').success(function(data){
-		value.data = data;
-	});
-}]);
+	})
+	.config(['$routeProvider', function($routeProvider) {
+	    $routeProvider.when('/rooms/:roomName', {
+	        controller: 'RoomController',
+	        controllerAs: 'room',
+	        templateUrl: 'static/detail.html'
+	    })
+	    .when('/sup', {
+	        controller: 'RoomAPIController',
+	        template: '<strong></strong>'
+	    });
+	}])
+    .controller('RoomController',['$http', '$routeParams', function($http, $routeParams){
+        console.log($routeParams);
+        var value = this;
+        value.title = [];
+        $http.get('/api/rooms/' + $routeParams.roomName).success(function(data){
+            value.title=data;
+            value.data = data;
+        });
+    }]);
